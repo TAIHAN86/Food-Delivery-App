84dd24bc180bd5e937a414938480f9645a483a36
taihan khane <taihankhan86@gmail.com>
2024-08-25 23:08:36 +0530

    redirect homepage

--- Frontend/app/(home)/index.js
+++ Frontend/app/(home)/index.js
@@ -1,12 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import {Redirect} from "expo-routerw"
 
 const index = () => {
   return (
-    
-     <Redirect href="/(home)"/>
-    
+    <View>
+      <Text>home screen</Text>
+      <Text>home screen</Text>
+    </View>
   )
 }
 
