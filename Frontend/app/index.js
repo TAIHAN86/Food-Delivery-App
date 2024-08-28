3dfb6c8340abc5ce78a2120e6af4c7145e65f19b
taihan khane <taihankhan86@gmail.com>
2024-08-28 15:46:39 +0530

    implemented expo location into homescreen

--- Frontend/app/index.js
+++ Frontend/app/index.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import {Redirect} from "expo-routerw"
+import {Redirect} from "expo-router"
 
 const index = () => {
   return (
