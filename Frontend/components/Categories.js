f734221708235664df83f0f13891a4107394f8ec
taihan khane <taihankhan86@gmail.com>
2024-09-01 22:31:41 +0530

    created a 3 array list which content image and dta for homescreen

--- Frontend/components/Categories.js
+++ Frontend/components/Categories.js
@@ -33,7 +33,7 @@ const Categories = () => {
     <View>
       <FlatList horizontal showsHorizontalScrollIndicator={false} data={items} renderItem={({item}) => (
           <TouchableOpacity activeOpacity={0.8} style={{marginTop:5}}>
-              <View style={{marginHorizontal:10,marginVertical:5,padding:5,backgroundColor:"#DB7093",borderRadius:4}}>
+              <View style={{marginHorizontal:10,marginVertical:5,padding:5,backgroundColor:"#1e90ff",borderRadius:4}}>
                   <Text style={{paddingHorizontal:5,color:"white",fontWeight:"500"}}>{item?.name}</Text>
               </View>
           </TouchableOpacity>
